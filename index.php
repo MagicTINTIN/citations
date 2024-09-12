dc44b6f4b27678c3c8fc420771139447d0469383
MagicTINTIN <valentinservieres@gmail.com>
2024-09-12 22:39:03 +0200

    list all messages

--- index.php
+++ index.php
@@ -1,68 +1,10 @@
 <?php session_start();
-
-if (isset($_POST["newsession"]) && isset($_POST["startsession"])) {
-    $_SESSION["name"] = htmlspecialchars($_POST["newsession"]);
-    $_SESSION["time"] = time();
-    $_SESSION["new"] = 0;
-    $_SESSION["updated"] = 0;
-    $_SESSION["total"] = 0;
-    header("Location: ./");
-    exit();
-}
-
-if (isset($_POST["end"])) {
-    unset($_SESSION["name"], $_SESSION["time"]);
-    header("Location: ./");
-}
-
+// include_once("includes/cas.php");
+$username = "user";//phpCAS::getUser();
 include_once("includes/db.php");
 include_once("includes/time.php");
-$outputvalue = "";
 
 if (isset($_POST["plate"]) && isset($_SESSION["name"]) && isset($_SESSION["time"]) && isset($_SESSION["new"]) && isset($_SESSION["updated"]) && isset($_SESSION["total"])) {
-    $platename = strtoupper(htmlspecialchars($_POST["plate"]));
-
-    $db = dbConnect();
-    $platesStatement = $db->prepare('SELECT * FROM plates WHERE plate = :plate');
-    $platesStatement->execute(['plate' => $platename]);
-    $plates = $platesStatement->fetchAll();
-
-
-    $isNewPlate = 1;
-    $newplatetype = 0;
-    $oldplatetype = 0;
-    $nbSeen = 1;
-    $now = time();
-    if (sizeof($plates) > 0) {
-        $isNewPlate = 0;
-        $oldplatetype = $plates[0]["type"];
-        $nbSeen += $plates[0]["nbSeen"];
-        $outputvalue .= "+ Seen " . getTime((int) $plates[0]["lastSeen"], $now) . " ago (Seen $nbSeen times)<br>";
-    } else {
-        $outputvalue .= "+ NEW <br>";
-    }
-
-    if (isset($_POST["sendnplate"])) {
-        // do nothing
-    } else if (isset($_POST["sendlplate"])) {
-        if ($oldplatetype != 7)
-            $outputvalue .= "-> Rented car<br>";
-        $newplatetype = 7;
-    } else if (isset($_POST["sendbplate"])) {
-        if ($oldplatetype != 5)
-            $outputvalue .= "-> Bus<br>";
-        $newplatetype = 5;
-    } else if (isset($_POST["sendpplate"])) {
-        if ($oldplatetype == 3) {
-            $isNewPlate = -1;
-            $outputvalue .= "ALREADY SEEN PARKED<br>";
-        } else
-            $outputvalue .= "-> Parked<br>";
-        $newplatetype = 3;
-    }
-    if (!isset($_POST["sendpplate"]) && $oldplatetype == 3) {
-        $outputvalue .= "-> Not parked<br>";
-    }
 
 
     if ($isNewPlate == 1) {
@@ -108,72 +50,43 @@ if (isset($_POST["plate"]) && isset($_SESSION["name"]) && isset($_SESSION["time"
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
     <meta name="viewport" content="user-scalable=no, initial-scale=1, maximum-scale=1, minimum-scale=1, width=device-width, height=device-height, target-densitydpi=device-dpi" />
-    <title>Immatricat</title>
+    <title>Citations Magistrales</title>
     <link href="vars.css" rel="stylesheet">
     <link href="styles.css" rel="stylesheet">
     <link href="importer/imports.css" rel="stylesheet">
     <meta name="author" content="MagicTINTIN">
-    <meta name="description" content="J'ai aucun problème avec les statistiques. J'arrête quand je veux.">
+    <meta name="description" content="Un site pour recenser les pépites entendues en CM">
 
     <link rel="icon" type="image/x-icon" href="images/favicon.png">
 
-    <meta property="og:title" content="Immatricat">
-    <meta property="og:description" content="J'ai aucun problème avec les statistiques. J'arrête quand je veux.">
+    <meta property="og:title" content="Citations Magistrales">
+    <meta property="og:description" content="Un site pour recenser les pépites entendues en CM">
 
-    <meta property="og:image" content="https://etud.insa-toulouse.fr/~serviere/Immatricat/images/favicon.png">
+    <meta property="og:image" content="https://etud.insa-toulouse.fr/~serviere/citations/images/favicon.png">
     <meta property="og:image:type" content="image/png">
-    <meta property="og:image:alt" content="Logo of Immatricat">
+    <meta property="og:image:alt" content="Logo of Citations Magistrales">
 </head>
 
 <body>
     <main>
-        <form id="seestatsform" method="post" action="stats/">
-            <input type="submit" id="seestats" name="gotostats" value="SEE STATISTICS >>" />
-        </form>
-        <?php
-        if (!isset($_SESSION["name"]) || !isset($_SESSION["time"]) || !isset($_SESSION["total"]) || !isset($_SESSION["updated"]) || !isset($_SESSION["new"])) {
-        ?>
-            <form id="createsession" method="post">
-                <input type="text" id="sessioninput" name="newsession" required maxlength="32" placeholder="pseudo" title="Enter a pseudo for your session">
-                <input type="submit" id="sessionsubmit" name="startsession" value="START" />
-            </form>
+        <ul>
+            <?php
+            $db = dbConnect();
+            $platesStatement = $db->prepare('SELECT * FROM citations');
+            $platesStatement->execute();
+            $plates = $platesStatement->fetchAll();
 
-            <form id="farmform" method="post" action="farm/">
-                <input type="submit" id="farmbutton" name="gotofarm" value="NEW INTERFACE >>" />
-            </form>
-            <?php if (isset($_SESSION["new"]) && isset($_SESSION["updated"]) && isset($_SESSION["total"])) { ?>
-                <div id="output"><?php echo "New : " . $_SESSION["new"] . "<br>Updated : " . $_SESSION["updated"] . "<br>Total : " . $_SESSION["total"] ?></div>
-            <?php }
-        } else {
-            ?>
-            <div id="stats"><?php echo "New : " . $_SESSION["new"] . " | Updated : " . $_SESSION["updated"] . " | Total : " . $_SESSION["total"] ?></div>
-            <form id="plateform" method="post">
-                <div id="multibtn">
-                    <div class="row">
-                        <input type="submit" id="platesubmitn" class="subplate subchoice" name="sendnplate" value="NORMAL" />
-                        <input type="submit" id="platesubmitb" class="subplate subchoice" name="sendbplate" value="BUS" />
-                    </div>
-                    <div class="row">
-                        <input type="submit" id="platesubmitl" class="subplate subchoice" name="sendlplate" value="RENTED" />
-                        <input type="submit" id="platesubmitp" class="subplate subchoice" name="sendpplate" value="PARKED" />
-                    </div>
-                </div>
-                <input type="text" id="plateinput" class="plateinput" name="plate" required pattern="^[A-Za-z]{3}\d{3}$" oldpattern="[a-zA-Z0-9_-]+" minlength="6" maxlength="6" size="6" title="Enter the car plate" placeholder="XXX000" autocomplete="off" autocorrect="off" autocapitalize="off" spellcheck="false">
-            </form>
-
-            <form id="endform" method="post">
-                <input type="submit" id="submitend" name="end" value="END SESSION" />
-            </form>
-            <!-- val "<?php echo $outputvalue ?>" -->
-            <?php if (isset($_SESSION["outputmsg"])) {
-            ?>
-                <div id="output"><?php echo $_SESSION["outputmsg"] ?></div>
-        <?php
-                unset($_SESSION["outputmsg"]);
+            foreach ($plates as $key => $value) {
+                echo "<li>
+                <div class='citationZone'><span class='citation'>\"" . $value["citation"] . "\"</div>
+                <div class='authorDateZone'><span class='authorDate'>" . $value["author"] . " - " . $value["date"] . "</div>";
+                if (in_array($username , array( 'serviere' , 'v_lasser')) || $username == $value["username"]) {
+                    echo "<div class='deleteCitation'>x</div>";
+                }
+                echo "</li>";
             }
-            echo '<script src="script.js"></script>';
-        }
-        ?>
+            ?>
+        </ul>
     </main>
 </body>
 
