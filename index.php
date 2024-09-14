c88f7e72f8a3e3d2797c40dcc56649fd11f521f4
MagicTINTIN <magictintin@proton.me>
2024-09-14 13:35:48 +0200

    moving safestr

--- index.php
+++ index.php
@@ -1,4 +1,8 @@
 <?php session_start();
+function safeStr($input) : string
+{
+    return str_replace("\n","\\n",str_replace("\\","\\\\",$input));
+}
 if (isset($_GET["json"])) {
 
     include_once("includes/db.php");
