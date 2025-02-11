0d939858231f94ad8cf4e360f2d00a636e3aaec1
MagicTINTIN <49071368+MagicTINTIN@users.noreply.github.com>
2025-02-11 09:49:19 +0100

    feat: mod view displays writer username

--- index.php
+++ index.php
@@ -24,10 +24,11 @@ if (isset($_GET["json"])) {
     echo "]";
     exit();
 }
-include_once("includes/cas.php");
+// include_once("includes/cas.php");
 $promoted = array('serviere', 'v_lasser', 'rebillar');
 $admin = array('serviere');
-$username = phpCAS::getUser();
+$username = "serviere";
+// $username = phpCAS::getUser();
 include_once("includes/db.php");
 include_once("includes/time.php");
 $db = dbConnect();
@@ -200,6 +201,13 @@ if (isset($_POST["citationInput"]) && isset($_POST["authorInput"]) && isset($_PO
                     if ((isset($_GET["mod"]) && $value["status"] >= 0) || (isset($_GET["ultradeleted"]) && $value["status"] < 0 && in_array($username, $admin)) || (isset($_GET["deleted"]) && $value["status"] == 0) || (isset($_GET["unverified"]) && $value["status"] == 1)) {
                         echo "<div class='citationZone zone'><span class='citation citationCommon'>\"" . $value["citation"] . "\"</div>
                         <div class='authorDateZone zone adzCitation'><span class='authorDate authorDateCommon'>" . $value["author"] . " - " . $value["date"] . "";
+                        if (isset($_GET["mod"]) && in_array($username, $promoted)) {
+                            ?>
+                            <div class="writer">
+                                <span>written by <?php echo $value['username']; ?></span>
+                            </div>
+                            <?php
+                        }
                         if (in_array($username, $promoted) || $username == $value["username"]) {
                             if (isset($_GET["ultradeleted"]) && $value["status"] < 0 && in_array($username, $admin)) { ?>
                                 <div class="delMsgDiv">
